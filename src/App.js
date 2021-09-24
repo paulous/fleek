9658c168d86e3e69dc0e104dc2320d0773766ba0
Paul Reid <paulous@gmail.com>
2021-09-24 12:26:09 +1000

    0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
+const Main = styled.div`
+  font-size:3rem;
+`
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Main>Paulous on Fleek!</Main>
     </div>
   );
 }
